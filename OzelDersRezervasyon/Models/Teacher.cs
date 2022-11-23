1e0c8338f6bd9975176a3e973fa981d50799943f
Gogle Öztaş <demiroztas06@hotmail.com>
2022-11-23 21:18:37 +0300

    teacher

---
+++ OzelDersRezervasyon/Models/Teacher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OzelDersRezervasyon.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Adı")]
+        public string TeacherName { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Soyadı")]
+        public string TeacherSurname { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("E-mail")]
+        public string TeacherEmail { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Şifre")]
+        [DataType(DataType.Password)]
+        public string TeacherPassword { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Şifre Tekrar")]
+        [DataType(DataType.Password)]
+        [Compare("TeacherPassword")]
+        public string TeacherPassword2 { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Şehir")]
+        public string TeacherCity { get; set; }
+        [Required(ErrorMessage = "Lütfen boş bırakmayınız")]
+        [DisplayName("Branş")]
+        public string TeacherBranch { get; set; }
+        [DisplayName("Fotoğraf")]
+        public HttpPostedFileBase TeacherPhoto { get; set; }
+    }
+}
